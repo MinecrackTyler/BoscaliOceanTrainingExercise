fa8a9ddef5e52357e721cacd1aace4a37480041e
MinecrackTyler <minecracktyler@gmail.com>
2026-08-23 17:19:02 -0500

    should allow player ships to rearm on the move again (thanks mitch?)

--- src/Patches/MiscPatches.cs
+++ src/Patches/MiscPatches.cs
@@ -216,6 +216,40 @@ public static class HangarPatches
     }
 }
 
+[HarmonyPatch(typeof(RearmMissionController))]
+public static class RearmMissionControllerPatches
+{
+    [HarmonyPatch(nameof(RearmMissionController.TryGetRearmer))]
+    [HarmonyPrefix]
+    public static bool TryGetRearmer_Prefix(RearmMissionController __instance, Unit requestingUnit, out Rearmer bestRearmer, ref bool __result)
+    {
+        bestRearmer = null;
+        if (requestingUnit.radarAlt > 1f || (requestingUnit.speed > 1f && !(requestingUnit is Ship || ModAssets.i.ShipDefinitions.Contains(requestingUnit.definition))))
+        {
+            __result = false;
+            return false;
+        }
+        GlobalPosition a = requestingUnit.GlobalPosition();
+        RearmMissionController.availableRearmersCache.Clear();
+        foreach (Rearmer rearmer in __instance.Rearmers)
+        {
+            if (rearmer.Unit != requestingUnit && rearmer.Capacity > 0f && FastMath.InRange(a, rearmer.GetPosition(), rearmer.Range))
+            {
+                RearmMissionController.availableRearmersCache.Add(rearmer);
+            }
+        }
+        if (RearmMissionController.availableRearmersCache.Count == 0)
+        {
+            __result = false;
+            return false;
+        }
+        RearmMissionController.availableRearmersCache.Sort((Rearmer rearmer, Rearmer b) => b.Capacity.CompareTo(rearmer.Capacity));
+        bestRearmer = RearmMissionController.availableRearmersCache[0];
+        __result = true;
+        return false;
+    }
+}
+
 [HarmonyPatch(typeof(Turret))]
 public static class TurretPatches
 {
